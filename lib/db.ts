b1fba6c007f5c8780d3bd8845b813fd394bf2c85
horaciospadaro-art <horaciospadaro@gmail.com>
2026-02-08 00:32:05 -0400

    fix: restore prisma v7 configuration with adapter pattern

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,17 @@
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const prismaClientSingleton = () => {
+    return new PrismaClient()
+}
 
-const pool = new Pool({ connectionString })
-// @ts-ignore
-const adapter = new PrismaPg(pool)
-const prisma = new PrismaClient({ adapter })
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
